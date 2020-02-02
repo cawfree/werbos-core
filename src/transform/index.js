3ef5406a432232a2925edcbb5e84ebfd925c8fa1
cawfree <hello@cawfree.com>
2020-02-02 21:35:29 +0000

    Generated normalized tensors.

--- src/transform/index.js
+++ src/transform/index.js
@@ -1,14 +1,31 @@
+import * as tf from '@tensorflow/tfjs';
 import { stdev, mean } from 'stats-lite';
 
 // https://en.wikipedia.org/wiki/Feature_scaling#Standardization_(Z-score_Normalization)
 const normalizeInputs = (inputs) => {
   const m = mean(inputs);
   const s = 1 / stdev(inputs);
-  return inputs
-    .map(e => (e - m) * s);
+  return new Float32Array(
+    inputs
+      .map(e => (e - m) * s),
+  );
 };
 
+//tf.tensor1d(inputs.map(input => normalizeInputs(input)))
+
 export const normalize = () => handle => [
-  handle('[Number]', input => normalizeInputs(input)),
-  handle('[[Number]]', inputs => inputs.map(input => normalizeInputs(input))),
+  handle('[Number]', input => tf.tensor1d(normalizeInputs(input))),
+  handle(
+    '[[Number]]',
+    (inputs) => {
+      const f = inputs.map(t => tf.tensor1d(t));
+      const data = tf.concat(f);
+      const { shape } = data;
+      return data
+        .reshape(
+          [inputs.length, shape[0] / inputs.length],
+        )
+        .transpose();
+    },
+  ),
 ] && undefined;
