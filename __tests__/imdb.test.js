19b18f0ad4648ef5779d4c69057159108c1c0424
cawfree <hello@cawfree.com>
2020-02-04 20:42:55 +0000

    Implemented symbol generation for words.

--- __tests__/imdb.test.js
+++ __tests__/imdb.test.js
@@ -15,7 +15,7 @@ it("should be capable of classifying imdb review sentiment", () => {
         [/$.*.s/],
       ],
     )
-    .use(oneHot(), oneHot());
+    .use(oneHot({ max: 1024 }), oneHot());
 
   const result = app('https://github.com/nas5w/imdb-data/raw/master/reviews.json');
 
--- src/tensor/index.js
+++ src/tensor/index.js
@@ -83,11 +83,47 @@ export const scalar = () => handle =>
     ),
   ] && undefined;
 
-export const oneHot = () => handle => [
-  handle(
-    "[[String]]", () => console.log('ici str'),
-  ),
-  handle(
-    "[[Number]]", () => console.log('ici number'),
-  ),
-] && undefined;
+const oneHotDefaultOptions = Object
+  .freeze(
+    {
+      max: Number.MAX_SAFE_INTEGER,
+    },
+  );
+
+const symbols = (src, max = Number.MAX_SAFE_INTEGER) =>
+  Array.from(
+    src.reduce((m, s) => {
+      m.set(s, (m.get(s) || 0) + 1);
+      return m;
+    }, new Map())
+  )
+    .sort(([w1, c1], [w2, c2]) => c2 - c1)
+    .map(([w]) => w)
+    .filter((_, i) => i < max);
+
+export const oneHot = (options = oneHotDefaultOptions) => (handle) => {
+  if (typeCheck('Object', options)) {
+    // TODO: Need smarter options handling, as well as per-type initialization.
+    const opts = {
+      ...oneHotDefaultOptions,
+      ...options,
+    };
+    const { max } = opts;
+    handle(
+      "[[String]]", (input) => {
+        const words = input
+          .reduce(
+            (arr, e) => arr.concat(...e.map(f => f.toLowerCase().match(/\w+\s+/g))),
+            [],
+          );
+        const sym = symbols(words, max);
+
+      },
+    );
+    handle(
+      "[[Number]]", () => console.log('ici number'),
+    );
+    return undefined;
+  }
+  throw new Error(`Expected [object Object], encountered ${options}.`);
+};
