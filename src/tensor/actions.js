127dd393b02663d7afcb61de7797a77dd8292b45
cawfree <hello@cawfree.com>
2020-02-08 17:48:13 +0000

    Abstracted activation props.

--- src/tensor/actions.js
+++ src/tensor/actions.js
@@ -3,8 +3,8 @@ import { createTensor } from "./model";
 import { RECEIVE_TENSOR } from "./actionTypes";
 
 // TODO: Throw if the user has supplied a "type" property.
-const receiveTensor = (tensor) => (dispatch, getState) => {
-  if (typeCheck('Object', tensor)) {
+const receiveTensor = tensor => (dispatch, getState) => {
+  if (typeCheck("Object", tensor)) {
     const { id } = tensor;
     if (typeCheck("String", id) && id.length > 0) {
       const { tensor: state } = getState();
@@ -19,12 +19,12 @@ const receiveTensor = (tensor) => (dispatch, getState) => {
   }
 };
 
-export const initializeTensors = () => (dispatch) => {
-  dispatch(receiveTensor(require('./defs/numeric-1d-normal.json')));
-  dispatch(receiveTensor(require('./defs/numeric-1d-scalar.json')));
-  dispatch(receiveTensor(require('./defs/numeric-2d-normal.json')));
-  dispatch(receiveTensor(require('./defs/numeric-2d-onehot.json')));
-  dispatch(receiveTensor(require('./defs/numeric-2d-scalar.json')));
-  dispatch(receiveTensor(require('./defs/numeric-2d-threshold.json')));
-  dispatch(receiveTensor(require('./defs/string-2d-onehot.json')));
+export const initializeTensors = () => dispatch => {
+  dispatch(receiveTensor(require("./defs/numeric-1d-normal.json")));
+  dispatch(receiveTensor(require("./defs/numeric-1d-scalar.json")));
+  dispatch(receiveTensor(require("./defs/numeric-2d-normal.json")));
+  dispatch(receiveTensor(require("./defs/numeric-2d-onehot.json")));
+  dispatch(receiveTensor(require("./defs/numeric-2d-scalar.json")));
+  dispatch(receiveTensor(require("./defs/numeric-2d-threshold.json")));
+  dispatch(receiveTensor(require("./defs/string-2d-onehot.json")));
 };
--- src/tensor/model.js
+++ src/tensor/model.js
@@ -2,7 +2,6 @@ import { Map } from "immutable";
 
 const defaultTensor = Object.freeze({});
 
-// TODO: Could add sanity checking for these props.
 export const createTensor = (props = defaultTensor) =>
   Object.freeze({ ...defaultTensor, ...props });
 
