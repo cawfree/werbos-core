13c03da69d1f7e40d1cad06b062ebfa2749bccec
cawfree <hello@cawfree.com>
2020-02-11 16:23:44 +0000

    Updated input props.

--- __tests__/boston.test.js
+++ __tests__/boston.test.js
@@ -31,7 +31,7 @@ it("should be capable of calculating regression using the boston dataset", () =>
     .use(normalize(), scalar())
     .use(
       sequential()
-        //.use(dense({ units: 10 }))
+        .use(dense({ units: 10 }))
         //.use(dense({ units: 20 }))
         //.use(dense())
     );
--- src/layer/defs/dense.js
+++ src/layer/defs/dense.js
@@ -1,1 +1,48 @@
-export default () => handle => handle("*", () => "i am a layer");
+import { typeCheck } from "type-check";
+import { layers } from '@tensorflow/tfjs';
+
+import { model } from '../../shape';
+
+const { dense } = layers;
+const defaultOptions = Object.freeze({});
+
+const getInputProps = (state, [tensor, tensorMeta]) => {
+  const { tensor: typeDef } = tensorMeta;
+  if (!typeCheck('String', typeDef)) {
+    throw new Error(`Expected tensor type definition, but encountered ${typeDef}.`);
+  }
+  const { tensor: model } = state;
+  const { activation } = model.get(typeDef);
+  if (typeCheck('String', activation)) {
+    const { shape } = tensor;
+    return { activation, inputShape: shape.slice(1) };
+  }
+  throw new Error(`Expected string activation, but encountered ${activation}.`);
+};
+
+const getTargetProps = (state, [tensor, targetMeta]) => {
+  return {};
+};
+
+export default (options = defaultOptions) => (handle, { getState }) => handle(model(getState()), (model, { useGlobal, useMeta, useTopology }) => {
+  const [index, length] = useTopology();
+  // XXX: The sequential() item will be the first entry in the middleware.
+  const firstLayer = index === 1;
+  const targetLayer = index === length - 1;
+  const [inputDef, targetDef] = useMeta();
+  const { getState } = useGlobal();
+  const state = getState();
+  model.add(
+    dense(
+      {
+        ...options,
+        ...(firstLayer ? getInputProps(state, inputDef) : {}),
+        ...(targetLayer ? getTargetProps(state, targetDef) : {}),
+      },
+    )
+  );
+
+  useMeta(useMeta());
+
+  return model;
+});
