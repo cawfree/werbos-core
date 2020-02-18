cc1aa814d7ab5e5093695fa2a68a1b64083f4c67
cawfree <hello@cawfree.com>
2020-02-18 19:14:41 +0000

    Abstracted loss and rectify calls.

--- src/train/defs/train.js
+++ src/train/defs/train.js
@@ -2,6 +2,7 @@ import { typeCheck } from "type-check";
 import { train } from "@tensorflow/tfjs";
 
 import { model } from "../../shape";
+import { loss, rectify } from "../model";
 
 const defaultOptions = Object.freeze({
   batchSize: 128,
@@ -9,40 +10,6 @@ const defaultOptions = Object.freeze({
   validationSplit: 0.0625,
   optimizer: train.rmsprop(1e-2)
 });
-
-const loss = (state, targetMeta) => {
-  const { tensor: typeDef } = targetMeta;
-  if (!typeCheck("String", typeDef)) {
-    throw new Error(
-      `Expected tensor type definition, but encountered ${typeDef}.`
-    );
-  }
-  const { tensor } = state;
-  const { loss } = tensor.get(typeDef);
-  if (!typeCheck("String", loss)) {
-    throw new Error(
-      `A loss function has not been specified for tensor "${typeDef}". Expected string, encountered ${loss}.`
-    );
-  }
-  return loss;
-};
-
-const rectify = (state, ys, targetMeta) => {
-  const { rectify } = state;
-  const { tensor: typeDef } = targetMeta;
-  if (!typeCheck("String", typeDef)) {
-    throw new Error(
-      `Expected tensor type definition, but encountered ${typeDef}.`
-    );
-  }
-  const shouldRectify = rectify.get(typeDef);
-  if (!typeCheck("Function", shouldRectify)) {
-    throw new Error(
-      `Expected function rectifier, but encountered ${shouldRectify}.`
-    );
-  }
-  return shouldRectify(ys, targetMeta);
-};
 
 export default (options = defaultOptions) => (handle, { getState }) =>
   handle(model(getState()), (model, { useMeta, useState, useGlobal }) => {
--- src/train/model.js
+++ src/train/model.js
@@ -1,3 +1,38 @@
+import { typeCheck } from "type-check";
 import { Map } from "immutable";
+
+export const loss = (state, targetMeta) => {
+  const { tensor: typeDef } = targetMeta;
+  if (!typeCheck("String", typeDef)) {
+    throw new Error(
+      `Expected tensor type definition, but encountered ${typeDef}.`
+    );
+  }
+  const { tensor } = state;
+  const { loss } = tensor.get(typeDef);
+  if (!typeCheck("String", loss)) {
+    throw new Error(
+      `A loss function has not been specified for tensor "${typeDef}". Expected string, encountered ${loss}.`
+    );
+  }
+  return loss;
+};
+
+export const rectify = (state, ys, targetMeta) => {
+  const { rectify } = state;
+  const { tensor: typeDef } = targetMeta;
+  if (!typeCheck("String", typeDef)) {
+    throw new Error(
+      `Expected tensor type definition, but encountered ${typeDef}.`
+    );
+  }
+  const shouldRectify = rectify.get(typeDef);
+  if (!typeCheck("Function", shouldRectify)) {
+    throw new Error(
+      `Expected function rectifier, but encountered ${shouldRectify}.`
+    );
+  }
+  return shouldRectify(ys, targetMeta);
+};
 
 export default Map();
