2e05fa730b5ff34900b0c312341ea3366275ad18
cawfree <hello@cawfree.com>
2020-03-03 10:14:39 +0000

    Removed transform meta.

--- src/meta/model.js
+++ src/meta/model.js
@@ -2,7 +2,6 @@ import { Map } from "immutable";
 
 import { id as Stimuli } from "./defs/stimuli";
 import { id as Tensor } from "./defs/tensor";
-import { id as Transform } from "./defs/transform";
 
 export const initialMeta = () => ({ useGlobal }) => {
   const { getState } = useGlobal();
@@ -13,13 +12,5 @@ export const initialMeta = () => ({ useGlobal }) => {
     return input;
   };
 };
-
-export const Meta = Object.freeze(
-  {
-    Stimuli,
-    Tensor,
-    Transform,
-  },
-);
 
 export default Map();
