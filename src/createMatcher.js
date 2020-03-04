1a0ed2cea7d5955e174750eaa11599ddf5ef2ee7
cawfree <hello@cawfree.com>
2020-03-04 13:25:06 +0000

    Removed buffer declaration in favour of raw type inspection. This better scales and will permit us to support wider image data in future.

--- src/createMatcher.js
+++ src/createMatcher.js
@@ -2,9 +2,5 @@ import { typeCheck } from "type-check";
 
 export default (...args) => typeCheck(
   ...args,
-  {
-    customTypes: {
-      Buffer: { typeOf: 'Uint8Array', validate: Buffer.isBuffer },
-    },
-  },
+  { customTypes: {} },
 );
--- src/transform/defs/buffer-2d-threshold.js
+++ src/transform/defs/buffer-2d-threshold.js
@@ -7,9 +7,26 @@ import { reshape2d } from "../model";
 
 export default () => (inputs, { useMeta }) => {
   const { [transformMeta]: { width, height, channels } } = useMeta();
+  // XXX: I believe this is compatible with tf.
+  const nextShape = [width, height, channels];
+  return inputs.map(
+    (arrayOfBuffers) => arrayOfBuffers
+      .map(
+        (someBuffer) => {
+          // okay, so we know this is (width * height)
+          const arrayData = [...someBuffer];
+        },
+      ),
+  );
+
+  
+
+  // converted an array of numbers into a buffer
+  // where [Number] was a full image 2d,
+  // "typeDef": "[[Number]]",
   // Need to use this information to correctly interpret image data.
   // How does sharp do it? Is it hardware agnostic? i.e. endianness?
-  throw 'we got here!';
+  //throw 'we got here!';
   // TODO: This might be wrong! It's a different implementation from /master.
   //       If low prediction rates occur, the older model should be reused.
   //return reshape2d(
