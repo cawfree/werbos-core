14137ed1ba1a3647c6d44bb552c8ee0f4d69b8d9
cawfree <hello@cawfree.com>
2020-03-04 00:58:52 +0000

    Check in prior to channel-wide normalization.

--- src/transform/defs/buffer-2d-threshold.js
+++ src/transform/defs/buffer-2d-threshold.js
@@ -1,9 +1,14 @@
 import { tensor1d, sub, scalar, sign, relu } from "@tensorflow/tfjs";
 import otsu from "otsu";
 
+import { id as transformMeta } from "../../meta/defs/transform";
+
 import { reshape2d } from "../model";
 
-export default () => inputs => {
+export default () => (inputs, { useMeta }) => {
+  const { [transformMeta]: { width, height, channels } } = useMeta();
+  // Need to use this information to correctly interpret image data.
+  // How does sharp do it? Is it hardware agnostic? i.e. endianness?
   throw 'we got here!';
   // TODO: This might be wrong! It's a different implementation from /master.
   //       If low prediction rates occur, the older model should be reused.
