adf342eb56992282fc4b6946835972fd93354d0b
cawfree <hello@cawfree.com>
2020-03-04 16:35:31 +0000

    Attempt at generating channel data.

--- src/transform/defs/uint8array-1d-threshold.js
+++ src/transform/defs/uint8array-1d-threshold.js
@@ -22,35 +22,23 @@ export default () => (inputs, { useMeta, useTensor }) => {
   const { [transformMeta]: { width, height, channels } } = useMeta();
 
   // XXX: I believe this is compatible with tf.
-  const nextShape = [width, height, channels];
+  //const nextShape = [width, height, channels];
 
   // XXX: Thresholded data only returns a single channel of data.
   useTensor({ channels: 1 });
 
   return inputs
     .map(buf => sumBuffer(buf, channels))
+    .map(input => tensor1d(input)
+      .sub(scalar(otsu(input)))
+      .sign()
+      .relu()
+      // Verify that this works.
+      .reshape([height, width, 1]),
+    );
     //.map(e => console.log(e.length,'vs',width*height));
 
   // 2352 / (28*28)
-  //return inputs.map(
-  //  (arrayOfBuffers) => arrayOfBuffers
-  //    .map(
-  //      (someBuffer) => {
-  //        // okay, so we know this is (width * height)
-  //        const arrayData = [...someBuffer];
-  //        //const tensorData = ;
-  //        //// width by width
-  //        //for (let i = 0; i < arrayData.length; i += 1) {
-  //        //
-  //        //}
-
-
-  //        // width, width, width (of chan)
-  //        // width, width, width (of chan)
-  //      },
-  //    ),
-  //);
-
   // converted an array of numbers into a buffer
   // where [Number] was a full image 2d,
   // "typeDef": "[[Number]]",
