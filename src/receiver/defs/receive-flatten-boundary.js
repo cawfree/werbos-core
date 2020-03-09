eaae3528b11f04274d6ddfcfa43f73d4a574bf00
cawfree <hello@cawfree.com>
2020-03-09 12:17:06 +0000

    Implemented layer connectivity test.

--- src/receiver/defs/receive-flatten-boundary.js
+++ src/receiver/defs/receive-flatten-boundary.js
@@ -4,14 +4,42 @@ const { Layer } = Variant;
 
 export const id = '_XZJdQ2orEy4NuMcDho35';
 
-export default (state, computeKeyFor, ...stages) => {
-  stages.map(
-    (stage) => {
-      const { variant: variantId } = computeKeyFor(stage);
-      if (variantId === Layer) {
-        console.log("Found a layer!");
-      }
-    },
-  );
+// XXX: Detects whether we need to insert a call to flatten() between two layers.
+const flattenBoundary = (state, computeKeyFor, ...stages) => {
+  for (let i = 1; i < stages.length; i += 1) {
+    const stage = stages[i];
+    const lastStage = stages[i - 1];
+
+    const { variant: variantId, ...extras } = computeKeyFor(stage);
+    const { variant: lastVariantId,  ...lastExtras } = computeKeyFor(lastStage);
+
+    if (variantId === Layer && lastVariantId === Layer) {
+      console.log('found two connecting layers');
+    }
+  }
+
   return stages;
 };
+
+export default flattenBoundary;
+
+//export default (state, computeKeyFor, ...stages) => {
+//  const stagesToReturn = [...stages];
+//  
+//  for (let i = 1; i < stagesToReturn.length; i += 1) {
+//    const lastStage = stagesToReturn[i - 1];
+//    const stage = stagesToReturn
+//
+//  }
+//
+//
+//  //stages.map(
+//  //  (stage) => {
+//  //    const { variant: variantId } = computeKeyFor(stage);
+//  //    if (variantId === Layer) {
+//  //      console.log("Found a layer!");
+//  //    }
+//  //  },
+//  //);
+//  //return stages;
+//};
