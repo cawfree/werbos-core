9ba504cc8e613285fd0f81caa1c877366e89e735
cawfree <hello@cawfree.com>
2020-03-14 10:16:33 +0000

    Prior to example implementation, pending migration of reusable network architecture.

--- __tests__/pretrained.test.js
+++ __tests__/pretrained.test.js
@@ -6,10 +6,17 @@ import werbos, { pretrained } from "../src";
 jest.setTimeout(24 * 60 * 60 * 1000);
 
 it("should load a graph model from tensorhub", async () => {
+
+  // const modelUrl =
+  //      'https://tfhub.dev/google/imagenet/mobilenet_v2_140_224/classification/2';
+  // const model = await tf.loadGraphModel(modelUrl, {fromTFHub: true});
+  // const zeros = tf.zeros([1, 224, 224, 3]);
+  // model.predict(zeros).print();
+
   const app = werbos()
     .use(
       //pretrained("https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v1_025_224/classification/1/default/1"),
-      pretrained('https://storage.googleapis.com/tfjs-models/savedmodel/mobilenet_v2_1.0_224/model.json'),
+      pretrained('https://tfhub.dev/google/imagenet/mobilenet_v2_140_224/classification/2'),
     );
   console.log(await app());
 });
